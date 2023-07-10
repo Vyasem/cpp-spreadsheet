9ecfbdd0d02d6cc183b6f68cc6b89a8331cabab6
Vyasem <vyasem@yandex.ru>
2023-07-10 17:35:46 +0300

    test separate & add test window

--- spreadsheet/main.cpp
+++ spreadsheet/main.cpp
@@ -1,7 +1,11 @@
 #include "gui/gui.h"
 #include "unit_test.h"
 
-int main(int argc, char** argv) {
+int test(int argc, char** argv){
     UnitTest testObj(argc, argv);
     return testObj.run();
+}
+
+int main(int argc, char** argv) {
+    return test(argc, argv);
  }
